6db0de652c2d27d779cfebf3a3f9232ae2813437
isholatosin <awoyele@hotmail.com>
2016-10-12 06:51:01 +0000

    Flarum all setup

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'c10',
+    'username' => 'isholatosin',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'http://chetu-corrected-isholatosin.c9users.io',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
